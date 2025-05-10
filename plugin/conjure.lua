9afdcba74ee570863fdae6214f1e957bd034193b
Niklas Dewally <niklas@dewally.com>
2025-05-10 12:52:52 +0100

    fix: typo in version check message

--- plugin/conjure.lua
+++ plugin/conjure.lua
@@ -1,5 +1,5 @@
 if 1 ~= vim.fn.has "nvim-0.11.0" then
-  vim.api.nvim_err_writeln "Telescope.nvim requires at least nvim-0.11.0."
+  vim.api.nvim_err_writeln "conjure.nvim requires at least nvim-0.11.0."
   return
 end
 
